6a349899775ec220c9da706311a949f0551729a0
Zhangxu0501 <81248397@qq.com>
2017-03-30 01:27:03 +0800

    update

--- src/jpeg/Main.java
+++ src/jpeg/Main.java
@@ -4,52 +4,75 @@ import java.io.DataOutputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 public class Main {
-	public static void main(String[] args){
-		String infile = "C:\\Users\\lining\\Desktop\\jpeg\\data\\lena.bmp";
-		String outfile = "C:\\Users\\lining\\Desktop\\jpeg\\data\\lena.txt";
+	public static void main(String[] args)throws Exception{
+		
+		int countrm=0;
+		int count_rm=0;
+		int countsm=0;
+		int count_sm=0;
+		
+		String infile = "/Users/zhangxu/workspace/lib/data/lenna.bmp";
 		int bit_len = 5;
 		BmpReader reader = new BmpReader();
 		
 		img TheImg = reader.getTheData(infile);
 		System.out.println(TheImg.width + " " + TheImg.height);
 		
-		int[] outdata = new int[(TheImg.width+8)*(TheImg.height+8)];
 		
 		int sx,sy;
 		int[][] temp = new int[8][8];
-		//try{
-			//OutputStream os = new FileOutputStream(outfile);  
-	        //DataOutputStream out = new DataOutputStream(os);  
+		int [][] temp1=null;
+		int [][]temp2=null;
 			for (int i = 0 ; i<TheImg.height/8+1; i++){
 				for (int j = 0; j<TheImg.width/8+1; j++){
-					sx = 8*i;
-					sy = 8*j;
-					for (int p = sx; p<sx+8; p++){
-						for (int q = sy; q<sy+8; q++){
-							if (p<TheImg.height && q<TheImg.width)
-								temp[p-sx][q-sy] = TheImg.data[q+p*TheImg.width];
-							else 
-								temp[p-sx][q-sy] = 0;
-						}
-					}
-					JPEG.DCTandSCH(temp);
-					for (int p = sx; p<sx+8; p++){
-						for (int q = sy; q<sy+8; q++){
-							//out.write(temp[p-sx][q-sy]);
-							//try{
-								outdata[q+p*TheImg.width] = temp[p-sx][q-sy];
-								F3alg.F3(outdata, bit_len, F3alg.secret(bit_len), TheImg.width, TheImg.height);
-							//}catch(ArrayIndexOutOfBoundsException e){
-							//	System.out.println("error");
-							//	System.out.println(q+"+"+p+"*"+TheImg.width);
-							//}
-						}
-					}
+					   		temp[i%8][j%8]=TheImg.data[i*TheImg.height+j];
+				}
+				if(i%8==0)
+				{
+					temp1=f1(temp.clone());
+			   		temp2=f_1(temp.clone());
+			   		double value=compute(temp);
+			   		double value1=compute(temp1);
+			   		double value2=compute(temp2);
+			   		
+			   		if(value<value1)
+			   			countrm+=1;
+			   		else
+			   			countsm+=1;
+			   		
+			   		if(value<value2)
+			   			count_rm+=1;
+			   		else
+			   			count_sm+=1;
 				}
 			}
-		//	out.close();
-		//}catch(Exception e){
-		//	System.out.println(e);
-		///}
+					
+			}
+	
+	public static int [] [] f1(int [] [] temp)
+	{
+		for(int i=0;i<8;i++)
+			for(int j=0;j<8;j++)
+			{
+				if(Integer.lowestOneBit(temp[i][j])==1)
+					temp[i][j]-=1;
+			}
+		return temp;
 	}
-}
+	public static int [][] f_1(int [] [] temp)
+	{
+		for(int i=0;i<8;i++)
+			for(int j=0;j<8;j++)
+			{
+				if(Integer.lowestOneBit(temp[i][j])!=1)
+					temp[i][j]-=1;
+			}
+		return temp;
+	}
+	public static double compute(int [][] temp)
+	{
+		return 0.0;
+	}
+		
+	}
+
